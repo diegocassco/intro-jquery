9eb92e3e7552e156390ec9bf169af5aa96fc2c1e
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-08-05 15:43:09 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+//zona segura
+document.addEventListener('DOMContentLoaded', function(){
+
+}
+
+
+
+
+document.addEventListener
+//recuperar por id
+
+debugger;
+const cedula = $('#txtCedula').val();
+// cambiar / asignar
+$('#txtNombre').val('Dario');
+alert(nombre)
+
+//jquery
+//zona segura
+$(function){
+    const nombre = $('#txtNombre').val();
+    const cedula = $('#txtCedula').val();
+
+}
+//cambiar al asignar
+$('#btnEnviar').on ('click,' function)
+//cuando hiciste click, se ejecuta aqui
+alert('hiciste')
+\\
